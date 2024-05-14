a4a0f04e99bf3c5bfa4da250175f87bde7f92ad1
François Ramon <francoisramon@mbp-de-francois.paris5.inserm.fr>
2024-05-14 11:30:30 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='pytracto',
     version='0.1',
+    author='François Ramon',
+    author_email='francois.ramon@orange.fr',
+    description='A python package for diffusion MRI tractography and connectivity, using interfaces of mrtrix3, fsl, freesurfer, ants, connectomemapper',
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+],
     packages=find_packages(),
-    install_requires=['python-on-whales>=0.71','pybids>=0.16','nipype>=1.8.6','dmri-amico>=2.0.1','connectomemapper>=3.1']
+    install_requires=['python-on-whales>=0.71',
+    'pybids>=0.16',
+    'nipype>=1.8.6',
+    'dmri-amico>=2.0.1',
+    'connectomemapper>=3.1']
 )
