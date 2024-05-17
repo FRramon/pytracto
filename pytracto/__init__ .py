fae74e1eeeab86605c94bafe08f0ba0c4a15186f
François Ramon <francoisramon@MacBook-Pro-de-Francois.local>
2024-05-17 09:44:14 +0200

    add a landing page

---
+++ pytracto/__init__ .py
@@ -0,0 +1,11 @@
+"""
+
+Python package for diffusion MRI tractography and connectivity
+
+Author : 'François Ramon'
+Mail : 'francois.ramon@ghu-paris.fr'
+
+Code on github : 'https://github.com/FRramon/pytracto'
+
+"""
+
