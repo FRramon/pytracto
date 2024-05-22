e7fe6ac92a1f7f70d30e13f795c79402024eb3e2
François Ramon <francoisramon@mbp-de-francois.paris5.inserm.fr>
2024-05-22 17:28:05 +0200

    change website theme

--- docs/conf.py
+++ docs/conf.py
@@ -34,5 +34,9 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_favicon = "_static/favicon.ico"
+html_static_path = ["_static"]
+html_logo = "_static/logo.png"
+
+html_css_files = ['theme_overrides.css']
