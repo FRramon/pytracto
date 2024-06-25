1a8bce1a72b38f708e6114fcdb551e849f6ccc55
Francois Ramon <francois_ramon@orange.fr>
2024-06-25 11:46:27 +0200

    add pydata

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ source_suffix = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'sphinx_rtd_theme'
-html_theme = 'pydata_sphinx_theme'
+#html_theme = 'pydata_sphinx_theme'
 
 html_static_path = ["_static"]
 
