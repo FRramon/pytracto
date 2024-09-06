7dd33fce8c4fc9d664958e05f2d26df5d71e18aa
François Ramon <francoisramon@mbp-de-francois.paris5.inserm.fr>
2024-09-06 14:34:08 +0200

    test pydata theme

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-#import pydata_sphinx_theme
+import pydata_sphinx_theme
 sys.path.insert(0, os.path.abspath(os.path.join('..', 'pytracto')))
 
 project = 'pytracto'
@@ -19,7 +19,6 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -36,7 +35,7 @@ source_suffix = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'sphinx_rtd_theme'
-#html_theme = 'pydata_sphinx_theme'
+html_theme = 'pydata_sphinx_theme'
 
 html_static_path = ["_static"]
 
